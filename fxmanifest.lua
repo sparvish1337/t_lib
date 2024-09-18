c9d934c329def67e90d94800d07b2dba85969b15
Theo <sparvish1337@gmail.com>
2024-09-18 21:21:27 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,21 +4,16 @@ game "gta5"
 lua54 "yes"
 use_experimental_fxv2_oal "yes"
 
-author "yiruzu"
-description "Cloud Service - Rental"
+author "Z"
+description "library"
 version "1.2.0"
-
-discord "https://discord.gg/jAnEnyGBef"
-repository "https://github.com/yiruzu/cloud-rental"
-license "CC BY-NC"
 
 shared_scripts { "@ox_lib/init.lua" }
 server_scripts { "server/*.lua" }
 
--- Ensure config.lua is loaded before client scripts
 client_scripts {
-    'config.lua',   -- Load config first
-    'client/*.lua'  -- Load all client scripts after
+    'config.lua',
+    'client/*.lua'
 }
 
 export 'ShowFloatingHelpText'
@@ -27,5 +22,5 @@ export 'ShowFloatingHelpText'
 
 
 
--- Example usage of the ShowFloatingHelpText export
--- exports['your_script_name']:ShowFloatingHelpText("Police Armory", 481.3466, -995.2707, 25.7297, 10.0)
+-- Example usage of helptext
+-- exports['t_lib']:ShowFloatingHelpText("Police Armory", 481.3466, -995.2707, 25.7297, 10.0)
